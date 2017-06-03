cafdd3c9dffaa08588cc7dbafd4eb523845a1268
rh389 <roberthogan@blueyonder.co.uk>
2017-06-03 17:33:01 +0100

    Add example to test suite

---
+++ test/integration/example.ts
@@ -0,0 +1,104 @@
+import * as ddbGeo from "../../";
+import * as AWS from "aws-sdk";
+import { expect } from "chai";
+
+AWS.config.update({
+    accessKeyId: '',
+    secretAccessKey: '',
+    region: 'eu-west-1'
+});
+
+describe('Example', function () {
+    // Use a local DB for the example.
+    const ddb = new AWS.DynamoDB({ endpoint: 'http://127.0.0.1:8000' });
+
+    // Configuration for a new instance of a GeoDataManager. Each GeoDataManager instance represents a table
+    const config = new ddbGeo.GeoDataManagerConfiguration(ddb, 'test-capitals');
+
+    // Instantiate the table manager
+    const capitalsManager = new ddbGeo.GeoDataManager(config);
+
+    // Use GeoTableUtil to help construct a CreateTableInput.
+    const createTableInput = ddbGeo.GeoTableUtil.getCreateTableRequest(config);
+
+    before(function () {
+        this.timeout(20000);
+        createTableInput.ProvisionedThroughput.ReadCapacityUnits = 2;
+        return ddb.createTable(createTableInput).promise()
+        // Wait for it to become ready
+            .then(function () {
+                return ddb.waitFor('tableExists', { TableName: config.tableName }).promise()
+            })
+            // Load sample data in batches
+            .then(function () {
+                console.log('Loading sample data from capitals.json');
+                const data = require('../../example/capitals.json');
+                const putPointInputs = data.map(function (capital, i) {
+                    return {
+                        RangeKeyValue: { S: String(i) }, // Use this to ensure uniqueness of the hash/range pairs.
+                        GeoPoint: {
+                            latitude: capital.latitude,
+                            longitude: capital.longitude
+                        },
+                        PutItemInput: {
+                            Item: {
+                                country: { S: capital.country },
+                                capital: { S: capital.capital }
+                            }
+                        }
+                    }
+                });
+
+                const BATCH_SIZE = 25;
+                const WAIT_BETWEEN_BATCHES_MS = 1000;
+                let currentBatch = 1;
+
+                function resumeWriting() {
+                    if (putPointInputs.length === 0) {
+                        return Promise.resolve();
+                    }
+                    const thisBatch = [];
+                    for (var i = 0, itemToAdd = null; i < BATCH_SIZE && (itemToAdd = putPointInputs.shift()); i++) {
+                        thisBatch.push(itemToAdd);
+                    }
+                    console.log('Writing batch ' + (currentBatch++) + '/' + Math.ceil(data.length / BATCH_SIZE));
+                    return capitalsManager.batchWritePoints(thisBatch).promise()
+                        .then(function () {
+                            return new Promise(function (resolve) {
+                                setInterval(resolve, WAIT_BETWEEN_BATCHES_MS);
+                            });
+                        })
+                        .then(function () {
+                            return resumeWriting()
+                        });
+                }
+
+                return resumeWriting().catch(function (error) {
+                    console.warn(error);
+                });
+            });
+    });
+
+    it('queryRadius', function () {
+        this.timeout(20000);
+        // Perform a radius query
+        return capitalsManager.queryRadius({
+            RadiusInMeter: 100000,
+            CenterPoint: {
+                latitude: 52.225730,
+                longitude: 0.149593
+            }
+        }).then((result) => {
+            expect(result).to.deep.equal([ { rangeKey: { S: '50' },
+                country: { S: 'United Kingdom' },
+                capital: { S: 'London' },
+                hashKey: { N: '522136' },
+                geoJson: { S: '{"type":"POINT","coordinates":[51.51,-0.13]}' },
+                geohash: { N: '5221366118452580119' } } ])
+        });
+    });
+
+    after(function () {
+        return ddb.deleteTable({ TableName: config.tableName }).promise()
+    });
+});
